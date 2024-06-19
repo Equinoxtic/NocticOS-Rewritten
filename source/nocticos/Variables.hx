6dcb28a9bbda9c929cf1561ef699b51aca99eded
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-19 12:17:29 +0800

    Update documentation for variables

--- source/nocticos/Variables.hx
+++ source/nocticos/Variables.hx
@@ -4,14 +4,34 @@ import nocticos.lib.HTTP;
 
 class Variables
 {
+	/**
+	 * The main application title. [ Default: NocticOS: Rewritten ]
+	 */
 	public static final APPLICATION_TITLE = "NocticOS: Rewritten";
 
+	/**
+	 * The application name that is used globally. [ Default: NocticOS ]
+	 */
 	public static final GLOBAL_APPLICATION_NAME = "NocticOS";
 
+	/**
+	 * The version of the application.
+	 */
 	public static final VERSION:String = FileIO.readFile(Paths.file('.VERSION'));
 
+	/**
+	 * The GIT version of the application. (Typically on GitHub)
+	 */
 	public static final GIT_VERSION:String = HTTP.getStringFromURL('https://raw.githubusercontent.com/Equinoxtic/NocticOS-Rewritten/master/.GIT_VERSION');
 
+	/**
+	 * The prompt of the command line. [ Default: '~$' ]
+	 */
+	public static final COMMAND_LINE_PROMPT:String = "~$";
+
+	/**
+	 * The string of every prompt. [ Default: [ Yes (Y) / No (N) ] ]
+	 */
 	public static final PROMPT_STRING:String = "[ Yes (Y) / No (N) ]";
 
 	/**
