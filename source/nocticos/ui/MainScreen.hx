3c184b38f1e2c6e310842fd6e159ddc81f508e16
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-20 23:27:59 +0800

    Update Main.hx

--- source/nocticos/ui/MainScreen.hx
+++ source/nocticos/ui/MainScreen.hx
@@ -10,7 +10,7 @@ class MainScreen extends Instance {
 	override function create():Void {
 		new TypedText('[ ${Variables.APPLICATION_TITLE} - v${Variables.VERSION} ]', DEFAULT, BLUE);
 		Sys.print("\n\n");
-		new TypedText('> Welcome to ${Variables.GLOBAL_APPLICATION_NAME} (${Variables.VERSION}). Type in \'help\' to\nget started.\n\n', DEFAULT, WHITE);
+		new TypedText('> Welcome to ${Variables.GLOBAL_APPLICATION_NAME} (${Variables.VERSION}). Type in \'help -a\' to\nget started.\n\n', DEFAULT, WHITE);
 		do {
 			Sys.print('${Variables.COMMAND_LINE_PROMPT} ');
 			new Input(null);
