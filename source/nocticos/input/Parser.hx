0007f59d1a16f8c6b5371895edf180996b5807ed
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-06-20 23:55:09 +0800

    Update Parser.hx

--- source/nocticos/input/Parser.hx
+++ source/nocticos/input/Parser.hx
@@ -21,6 +21,14 @@ class Parser {
 
 		var sCmd = flags[0];
 
+		#if (debug)
+		for (flag in 0...flags.length) {
+			if (flags[flag+1] != null) {
+				Sys.stdout().writeString('${sCmd} flag: ${flags[flag+1]}\n');
+			}
+		}
+		#end
+
 		currentCommand = sCmd;
 
 		CommandCalls.parseCommand(sCmd, flags);
