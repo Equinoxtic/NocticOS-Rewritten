67def13fd097c758c4b86fb63097b9de88292a96
Equinoxtic <thisistherealequinoctic@gmail.com>
2024-06-22 23:22:55 +0800

    Update TypedCommandText.hx

--- source/nocticos/ui/display/TypedCommandText.hx
+++ source/nocticos/ui/display/TypedCommandText.hx
@@ -1,51 +1,55 @@
 package nocticos.ui.display;
 
+import nocticos.types.StringBuffer;
 import nocticos.ui.display.text.TypedText;
 import nocticos.util.CommandUtility;
 
 using StringTools;
 
 class TypedCommandText {
+	/**
+	 * Create a text for commands with the type effect.
+	 * @param name The name of the command.
+	 * @param aliases The array of aliases of the command.
+	 * @param flags The array of flags of the command.
+	 * @param description The description of the command.
+	 */
 	public function new(name:String, aliases:Array<String>, flags:Array<String>, description:String):Void {
 		new TypedText(_formatCommand(
 			name,
 			aliases,
 			flags,
 			description
-		), TextType.DEFAULT_FAST, WHITE);
+		), TextType.QUICK, WHITE);
 
 		Sys.print('\n\n');
 	}
 
 	private function _formatCommand(name:String, aliases:Array<String>, flags:Array<String>, description:Null<String>):String {
-		var sbuf:String = "";
+		var stringBuffer:StringBuffer = new StringBuffer();
 
-		sbuf += '* [ $name ] ';
+		stringBuffer.write('* [ $name ]', WriteMode.APPEND);
 
-		if (aliases != null && aliases != null) {
+		if (aliases != null && flags != null) {
 			if (aliases.length > 0 && aliases[0] != "NO_ALIASES") {
-				var key:String = '${CommandUtility.appendStringArray(aliases, ' | ')}';
-				sbuf += Std.string(' // ( A: $key ) ');
-			} else {
-				sbuf += ' // (...)';
+				var aliasesString:String = '${CommandUtility.appendStringArray(aliases, ' | ')}';
+				stringBuffer.write(' // [ A: $aliasesString ]', WriteMode.APPEND);
 			}
 
 			if (flags.length > 0 && flags[0] != "NO_FLAGS") {
-				var key:String = '${CommandUtility.appendStringArray(flags, ', ')}';
-				sbuf += Std.string(' < $key >');
-			} else {
-				sbuf += ' < ... >';
+				var flagsString:String = '${CommandUtility.appendStringArray(flags, ', ')}';
+				stringBuffer.write(' : ( $flagsString )', WriteMode.APPEND);
 			}
 		}
 
-		sbuf += '\n';
+		stringBuffer.write('\n', WriteMode.APPEND);
 
-		if (description == "" && description.length < 0) {
-			description = "Command description";
+		if (description == "" || description.length <= 0) {
+			description = "< Command Description >";
 		}
 
-		sbuf += '  > \"$description\"';
+		stringBuffer.write('  -> ${StringBuffer.quotifyString(' $description ')}', WriteMode.APPEND);
 
-		return sbuf;
+		return stringBuffer.read();
 	}
 }
