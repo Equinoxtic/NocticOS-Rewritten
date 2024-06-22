441cef0c2bacb5bda5fde1f784629008c412992e
Equinoxtic <thisistherealequinoctic@gmail.com>
2024-06-22 21:32:31 +0800

    Update MainScreen.hx

--- source/nocticos/ui/MainScreen.hx
+++ source/nocticos/ui/MainScreen.hx
@@ -11,11 +11,14 @@ class MainScreen extends Instance {
 	override function create():Void {
 		new TypedText('[ ${Variables.APPLICATION_TITLE} - v${Variables.VERSION} ]', DEFAULT, BLUE);
 		Sys.print("\n\n");
+		
 		new TypedText('> Welcome to ${Variables.GLOBAL_APPLICATION_NAME} (${Variables.VERSION}). Type in \'help -a\' to\nget started.\n\n', DEFAULT, WHITE);
+		
 		do {
 			Sys.print('${Variables.COMMAND_LINE_PROMPT} ');
 			new Input(null);
 		} while (true);
+		
 		super.create();
 	}
 }
