8457461041f66a8b5c1393cf2acc686e0299ddd7
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-13 00:18:14 +0800

    Update Logging.hx

--- source/nocticos/lib/Logging.hx
+++ source/nocticos/lib/Logging.hx
@@ -1,16 +1,10 @@
 package nocticos.lib;
 
-import backend.ANSI;
-import nocticos.util.StringFormatter;
-
 using nocticos.util.StringUtil;
 using StringTools;
 
 class Logging {
 	public static function logMessage(?message:String):Void {
-		FileStream.writeFile('logs/latest.txt', '{0}: {1}\n'.format([
-			StringFormatter.mappedSurround(Std.string(Date.now()), ['[', ']']),
-			message
-		]));
+		FileStream.writeFile('logs/latest.txt', '[${Date.now()}]: ${message}\n');
 	}
 }
