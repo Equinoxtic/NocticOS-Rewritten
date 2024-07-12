d7c4c83a520ab166d30c9842427703453f063f94
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-13 00:05:14 +0800

    FileStream.hx rewrite and updates

--- source/backend/FileStream.hx
+++ source/backend/FileStream.hx
@@ -1,5 +1,8 @@
 package backend;
 
+import nocticos.lib.Colorizer.Color;
+import nocticos.util.StringFormatter;
+import nocticos.types.StringBuffer.WriteMode;
 import haxe.io.Bytes;
 import sys.io.FileOutput;
 import nocticos.util.debug.Error;
@@ -7,6 +10,9 @@ import sys.FileSystem;
 import haxe.io.Eof;
 import sys.io.File;
 import sys.io.FileInput;
+
+using StringTools;
+using nocticos.util.StringUtil;
 
 enum FileWriteMode {
 	/**
@@ -35,6 +41,11 @@ enum FileWriteMode {
 	DOUBLE;
 }
 
+enum ItemType {
+	FILE;
+	DIRECTORY;
+}
+
 class FileStream extends File {
 	/**
 	 * Writes to the file, given the content and the specific mode for writing.
@@ -46,24 +57,36 @@ class FileStream extends File {
 		if (path == null || path == "") {
 			return;
 		}
+		var file:FileOutput = File.append(path, false);
+		if (file != null) {
+			if (writeMode == null) {
+				writeMode = FileWriteMode.STRING;
+			}
+			switch(writeMode) {
+				case STRING:
+					file.writeString(s);
+				case BYTES:
+					file.writeByte(Std.parseInt(s));
+				case INT:
+					file.writeInt32(Std.parseInt(s));
+				case FLOAT:
+					file.writeFloat(Std.parseFloat(s));
+				case DOUBLE:
+					file.writeDouble(Std.parseFloat(s));
+			}
+		}
+		file.close();
+		return;
+	}
+
+	public static function clearFile(path:String):Void {
+		if (path == null || path == "") {
+			return;
+		}
 		if (FileSystem.exists(path)) {
 			var file:FileOutput = File.write(path, false);
 			if (file != null) {
-				if (writeMode == null) {
-					writeMode = FileWriteMode.STRING;
-				}
-				switch(writeMode) {
-					case STRING:
-						file.writeString(s);
-					case BYTES:
-						file.writeByte(Std.parseInt(s));
-					case INT:
-						file.writeInt32(Std.parseInt(s));
-					case FLOAT:
-						file.writeFloat(Std.parseFloat(s));
-					case DOUBLE:
-						file.writeDouble(Std.parseFloat(s));
-				}
+				file.writeString('');
 			}
 			file.close();
 		}
@@ -82,5 +105,20 @@ class FileStream extends File {
 			Error.throwError(NO_PATH_EXISTS, 'Path does not exist! [$path]', true, true);
 		}
 		return line;
+	}
+
+	public static function newItem(path:Null<String>, ?type:Null<ItemType> = ItemType.FILE):Void {
+		if (path == null || path.trim() == '' || type == null) {
+			return;
+		}
+		switch(type) {
+			case FILE:
+				writeFile(path, '', FileWriteMode.STRING);
+			case DIRECTORY:
+				FileSystem.createDirectory(path);
+		}
+		Sys.print('Created: \"{0}\"'.format([
+			StringFormatter.color(path, Color.YELLOW)
+		]));
 	}
 }
