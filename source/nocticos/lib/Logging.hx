d8d3544fed99efa49c263b40e318c3cb488c6af6
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-17 13:43:15 +0800

    Update Logging.hx

--- source/nocticos/lib/Logging.hx
+++ source/nocticos/lib/Logging.hx
@@ -7,7 +7,9 @@ class Logging {
 	private static final LOG_FILE:String = 'logs/latest.txt';
 
 	public static function logMessage(?message:String):Void {
-		FileStream.writeFile(LOG_FILE, '[${Date.now()}]: ${message}\n');
+		FileStream.writeFile(LOG_FILE, '[{0}]: {1}\n'.format([
+			Std.string(Date.now()), message
+		]));
 	}
 
 	public static function readLogs():Void {
