5442993ef05520d7d60bac928b4b3f91ebf2ab8a
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-17 20:58:04 +0800

    Update ShellRenderer.hx

--- source/nocticos/ui/ShellRenderer.hx
+++ source/nocticos/ui/ShellRenderer.hx
@@ -8,9 +8,17 @@ import nocticos.lib.Colorizer.Color;
 using nocticos.util.StringUtil;
 
 class ShellRenderer {
+	/**
+	 * Stages the properties/attributes of the shell.
+	 * @param symbol The symbol/character of the shell.
+	 * @param id The ID of she shell.
+	 * @param color The color of the symbol/character.
+	 * @param hasBackground Whether the symbol should render a background.
+	 * @param backgroundColor The background color of the symbol/character
+	 */
 	public static function stage(symbol:Null<String>, id:Null<Int>, ?color:Null<Color> = WHITE, ?hasBackground:Bool = false, ?backgroundColor:Null<Color> = CYAN):Void {
 		Logging.logMessage('Staged and Registered symbol \"{0}\" with ID: {1}'.format([
-			symbol, id + 100
+			symbol, '00${Std.string(id)}'
 		]));
 		if (color != null) {
 			if (!hasBackground) {
