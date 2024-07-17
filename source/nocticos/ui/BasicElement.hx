b9d358ed9b703c0143395e9f6d0ac66d63d1ed92
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-17 20:58:19 +0800

    UpUpdate BasicElement.hx

--- source/nocticos/ui/BasicElement.hx
+++ source/nocticos/ui/BasicElement.hx
@@ -7,8 +7,16 @@ import nocticos.lib.io.StandardOutput;
 class BasicElement {
 	private var _elementProperties:Map<String, Dynamic> = [];
 
+	/**
+	 * Creates a basis of an element.
+	 */
 	public function new():Void {}
 
+	/**
+	 * Pushes a given property of the element, can be useful for elements that need to be reused again at some point.
+	 * @param key The key of the property.
+	 * @param value The value of the property.
+	 */
 	public function pushProperty(key:String, value:Dynamic):Void {
 		if (key == null || key.charAt(0) == '' || value == null) {
 			return;
@@ -19,6 +27,11 @@ class BasicElement {
 		_elementProperties.set(key, value);
 	}
 
+	/**
+	 * Gets the property of an element.
+	 * @param key The key of the element's property.
+	 * @return Dynamic
+	 */
 	public function getProperty(key:String):Dynamic {
 		if (key == null || key.charAt(0) == '') {
 			return null;
