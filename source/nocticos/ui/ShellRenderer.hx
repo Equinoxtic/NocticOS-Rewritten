1073d5dc219047831a5bb326e7b2b328bf0e698c
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-07-18 20:30:41 +0800

    Update ShellRenderer.hx

--- source/nocticos/ui/ShellRenderer.hx
+++ source/nocticos/ui/ShellRenderer.hx
@@ -16,16 +16,12 @@ class ShellRenderer {
 	 * @param hasBackground Whether the symbol should render a background.
 	 * @param backgroundColor The background color of the symbol/character
 	 */
-	public static function stage(symbol:Null<String>, id:Null<Int>, ?color:Null<Color> = WHITE, ?hasBackground:Bool = false, ?backgroundColor:Null<Color> = CYAN):Void {
+	public static function stage(symbol:Null<String>, id:Null<Int>, ?color:Null<Color> = WHITE):Void {
 		Logging.logMessage('Staged and Registered symbol \"{0}\" with ID: {1}'.format([
 			symbol, '00${Std.string(id)}'
 		]));
 		if (color != null) {
-			if (!hasBackground) {
-				Sys.print(StringFormatter.color(symbol, color, false));
-			} else {
-				Sys.print(StringFormatter.highlight(symbol, color, backgroundColor));
-			}
+			Sys.print(StringFormatter.color(symbol, color, false));
 		} else {
 			Sys.print(symbol);
 		}
