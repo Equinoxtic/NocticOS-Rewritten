810adc8e790a425a7dc8476028a65ed5323d22cc
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-09-13 23:13:10 +0800

    [Patch]: Fix commands looping through over iterations that causes most of the problems in this rewrite

--- source/nocticos/ui/command/CommandCalls.hx
+++ source/nocticos/ui/command/CommandCalls.hx
@@ -1,45 +1,30 @@
 package nocticos.ui.command;
 
 import nocticos.lib.StringFunctions;
-import nocticos.lib.Logging;
-import backend.System;
-import nocticos.commands.Exit;
-import nocticos.commands.Help;
 import nocticos.ui.command.Command;
 
 class CommandCalls {
 	public static function parseCommand(baseCommand:String, flags:Array<String>):Void {
-		/* switch(baseCommand) {
-			case "help" | "cmds":
-				Command.insantiateCommand(new Help(flags));
+		var currentCommand:CommandProperties = null;
 
-			case "exit" | "quit" | "q":
-				Command.insantiateCommand(new Exit(flags));
+		for (i in 0 ... Command.commandsList.length) {
+			currentCommand = Command.commandsList[i];
+			if (StringFunctions.compare(baseCommand, currentCommand.name)) {
+				break;
+			}
+		}
 
-			case "reload" | "refresh" | "rbuf" | "refbuf":
-				System._CLRSCR();
-				Instance.instantiate(new nocticos.ui.states.MainScreen());
-
-			case "clear" | "clr" | "cls" | "clrscr":
-				System._CLRSCR();
-
-			case "logs":
-				System._CLRSCR();
-				Logging.readLogs();
-
-			default:
+		if (currentCommand != null) {
+			if (StringFunctions.compare(baseCommand, currentCommand.name)) {
+				Command.insantiateCommand(currentCommand.commandClass, flags);
+			} else {
 				Instance.instantiate(new nocticos.ui.states.InvalidCommandState());
-		} */
-
-		for (k => v in Command.commandsList) {
-			if (StringFunctions.compare(baseCommand, v.name)) {
-				Command.insantiateCommand(v.commandClass, flags);
 			}
 
-			for (i in 0 ... v.aliases.length) {
-				var alias = v.aliases[i];
+			for (i in 0 ... currentCommand.aliases.length) {
+				var alias = currentCommand.aliases[i];
 				if (StringFunctions.compare(baseCommand, alias)) {
-					Command.insantiateCommand(v.commandClass, flags);
+					Command.insantiateCommand(currentCommand.commandClass, flags);
 				}
 			}
 		}
