65a4a4bc6bbde47c7a801067361ceae056c793b5
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-09-14 22:07:33 +0800

    [Feat]: Rewrite code to check for conflicting IDs in StringID.hx

--- source/nocticos/lib/StringID.hx
+++ source/nocticos/lib/StringID.hx
@@ -1,34 +1,71 @@
 package nocticos.lib;
 
+import nocticos.types.StringBuffer;
+import nocticos.util.debug.Error;
 using StringTools;
 
 class StringID {
+	private static var ASSIGNED_IDS:Array<Int> = [ 0 ];
+
 	public static function generate(id:Null<Int>):String {
 		if (id == null) {
 			return null;
 		}
 
-		var seperatedIndexes:Array<Int> = [];
+		var existingID:Bool = false;
+		var conflictingID:Int = 0;
+
+		for (i in 0 ... ASSIGNED_IDS.length) {
+			existingID = (id == ASSIGNED_IDS[i]);
+			conflictingID = ASSIGNED_IDS[i];
+			if (existingID) {
+				break;
+			}
+		}
+
+		if (existingID) {
+			_crashOS(id, conflictingID);
+		} else {
+			_assignID(id);
+		}
+
+		return _separateIndexes(id);
+	}
+
+	@:noPrivateAccess
+	private static function _separateIndexes(id:Int):String {
+		var indexes:Array<Int> = [];
 
 		if (id >= 1000) {
-			seperatedIndexes.push(_calculate(id, 1000));
+			indexes.push(_calculate(id, 1000));
 		}
+		indexes.push(_calculate(id, 100));
+		indexes.push(_calculate(id, 10));
+		indexes.push(_calculate(id, 1));
 
-		seperatedIndexes.push(_calculate(id, 100));
-		seperatedIndexes.push(_calculate(id, 10));
-		seperatedIndexes.push(_calculate(id, 1));
-
-		var r:String = '';
-
-		for (i in 0 ... seperatedIndexes.length) {
-			r += Std.string(seperatedIndexes[i]);
+		var buf:StringBuffer = new StringBuffer();
+		for (i in 0 ... indexes.length) {
+			buf.write(Std.string(indexes[i]));
 		}
-
-		return r;
+		return buf.read();
 	}
 
 	@:noPrivateAccess
 	private static function _calculate(id:Int, v:Int):Int {
 		return (Math.floor(id / v) % 10);
+	}
+
+	@:noPrivateAccess
+	private static function _assignID(id:Int):Void {
+		ASSIGNED_IDS.push(id);
+		Logging.logMessage('Assigned ID: ${Std.string(id)}');
+	}
+
+	@:noPrivateAccess
+	private static function _crashOS(id:Int, conflictingID:Int):Void {
+		var idString:String = _separateIndexes(id);
+		var idStringConflicted:String = _separateIndexes(conflictingID);
+		Logging.logMessage('ID $idString already exists! (CONFLICT)');
+		Error.throwError(ExceptionType.ID_CONFLICT, 'ID \'$idString\' conflicts with ID \'$idStringConflicted\' as it already exists.');
 	}
 }
