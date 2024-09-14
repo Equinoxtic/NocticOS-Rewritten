f073086b1dd8106b9a0bf48c837fb972d2cffeda
Vertic <86795271+Equinoxtic@users.noreply.github.com>
2024-09-14 22:00:49 +0800

    [Feat]: Add 'clearScreen' parameter for _EXIT method in System.hx

--- source/backend/System.hx
+++ source/backend/System.hx
@@ -1,16 +1,14 @@
 package backend;
 
 class System {
-	public static function _EXIT(?code:Int = 0):Void {
-		_CLRSCR();
+	public static function _EXIT(?code:Int = 0, ?clearScreen:Bool = false):Void {
+		if (clearScreen) {
+			_CLRSCR();
+		}
 		Sys.exit(code);
 	}
 
 	public static function _CLRSCR():Void {
-		#if !linux
-		Sys.command("cls", null);
-		#else
-		Sys.command("clear", null);
-		#end
+		Sys.command(#if (!linux) 'cls' #else 'clear' #end, null);
 	}
 }
